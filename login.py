6dad06ca4b5050ad72fd36632a1f041e86866df3
BORED <105173245+MicahBill@users.noreply.github.com>
2022-05-27 17:39:48 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from multiprocessing.connection import Client
 from linepy import *
 from akad.ttypes import OpType, Message, TalkException
 from threading import Thread
@@ -11,7 +10,7 @@ fileName = os.path.splitext(os.path.basename(__file__))[0]
 db = livejson.File("token/%s.json" % fileName)
 
 if ":" in db['token']:
-    app = "ANDROIDLITE\t2.11.1\tAndroid OS\t5.1.1"
+    app = "ANDROID\t2.11.1\tAndroid OS\t5.1.1"
 else:
     app = "DESKTOPWIN\t5.21.3\tWindows\t10"
 try:
@@ -19,13 +18,13 @@ try:
 except:
     e = traceback.format_exc()
     if "code=20" in e:print("FREEZING");time.sleep(3600);python3 = sys.executable;os.execl(python3, python3, *sys.argv)
-    elif "code=8" in e or "code=7" in e:LINE = LINE(db["mail"],db["pass"],certificate='{}.crt'.format(db["mail"]),appName=app);db['token'] = LINE.authToken
+    elif "code=8" in e or "code=7" in e:client = LINE(db["mail"],db["pass"],certificate='{}.crt'.format(db["mail"]),appName=app);db['token'] = client.authToken
     else:traceback.print_exc()
     
 
-uid = LINE.profile.mid
-poll = OEPoll(LINE)
-good = commands(fileName, LINE, app, uid)
+uid = client.profile.mid
+poll = OEPoll(client)
+good = commands(fileName, client, app, uid)
 print("LOGIN SUCCESS")
 def main_loop(op):
     if op.type == OT.RECEIVE_MESSAGE:good.receive_message(op)
@@ -40,9 +39,9 @@ def main_loop(op):
 
 while 1:
     try:
-        ops = LINE.poll.fetchOperations(LINE.revision, 50)
+        ops = client.poll.fetchOperations(client.revision, 50)
         for op in ops:
-            LINE.revision = max(LINE.revision, op.revision)
+            client.revision = max(client.revision, op.revision)
             t1 = Thread(target=main_loop(op,))
             t1.start()
             t1.join()
